7e9398a363878dcd31deb2cd71ebeef7784d9ac1
KamiteAkira0611 <kamiteakira@gmail.com>
2021-08-09 05:18:35 +0900

    save

--- golang/main.go
+++ golang/main.go
@@ -9,25 +9,28 @@ import (
 
 func main() {
   http.HandleFunc("/", indexHandler)
+  http.HandleFunc("/tasks", getTasks)
 
   port := os.Getenv("PORT")
   if port == "" {
-          port = "8080"
-          log.Printf("Defaulting to port %s", port)
+    port = "3000"
+    log.Printf("Defaulting to port %s", port)
   }
 
   log.Printf("Listening on port %s", port)
   if err := http.ListenAndServe(":"+port, nil); err != nil {
-          log.Fatal(err)
+    log.Fatal(err)
   }
 }
 
-
-// indexHandler responds to requests with our greeting.
 func indexHandler(w http.ResponseWriter, r *http.Request) {
   if r.URL.Path != "/" {
     http.NotFound(w, r)
     return
   }
   fmt.Fprint(w, "Hello, World!")
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, "Hello, tasks!")
 }
