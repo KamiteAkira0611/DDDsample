c6bf4c777ffa38217290550dcefc8312dfdfd78e
KamiteAkira0611 <kamiteakira@gmail.com>
2021-08-10 00:48:45 +0900

    save

--- golang/main.go
+++ golang/main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"cloud.google.com/go/datastore"
 	"github.com/gin-gonic/gin"
@@ -20,10 +19,17 @@ func main() {
   r.DELETE("/task/:taskid", deleteTask)
   r.POST("/task", createTask)
 
-  port = "3000"
+  port := "3000"
 
+  log.Printf("Listening on port %s", port)
   entryPoint := fmt.Sprintf("0.0.0.0:%s", port)
 	r.Run(entryPoint)
+}
+
+func getDatastoreClient(ctx context.Context) (client *datastore.Client, err error) {
+	projectID := os.Getenv("DATASTORE_PROJECT_ID")
+	client, err = datastore.NewClient(ctx, projectID)
+	return
 }
 
 func index(gc *gin.Context) {
@@ -31,7 +37,14 @@ func index(gc *gin.Context) {
 }
 
 func getTask(gc *gin.Context) {
-  gc.String(http.StatusOK, "hello gin!")
+  ctx := context.Background()
+
+  client, err := getDatastoreClient(ctx)
+  if err != nil {
+		gc.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+  log.Printf("client %s", client)
 }
 
 func deleteTask(gc *gin.Context) {
