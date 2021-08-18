b5c6df3b31f0964f42e14ad69a9554572258d2c3
KamiteAkira0611 <kamiteakira@gmail.com>
2021-08-18 17:28:21 +0900

    save

--- golang/main.go
+++ golang/main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 
 	"cloud.google.com/go/datastore"
-  // "google.golang.org/appengine"
+  "google.golang.org/appengine"
 )
 
 type Task struct {
@@ -18,7 +18,7 @@ type Task struct {
 var datastoreClient *datastore.Client
 
 func init()  {
-  http.HandleFunc("/", indexHandler)
+  http.HandleFunc("/", handleRoot)
   
   http.HandleFunc("/goon/get_task", getGoonTask)
   
@@ -42,10 +42,21 @@ func main() {
   // appengine.Main()
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-  if r.URL.Path != "/" {
-    http.NotFound(w, r)
-    return
-  }
-  fmt.Fprint(w, "Hello, World!")
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "Hello, GAE/go")
+  c := appengine.NewContext(r)
+  fmt.Fprintf(w, "[AppID]: %v\n", appengine.AppID(c))
+  fmt.Fprintf(w, "[Datacenter]: %v\n", appengine.Datacenter(c))
+  fmt.Fprintf(w, "[DefaultVersionHostname]: %v\n", appengine.DefaultVersionHostname(c))
+  fmt.Fprintf(w, "[InstanceID]: %v\n", appengine.InstanceID())
+  fmt.Fprintf(w, "[IsAppEngine]: %v\n", appengine.IsAppEngine())
+  fmt.Fprintf(w, "[IsDevAppServer]: %v\n", appengine.IsDevAppServer())
+  fmt.Fprintf(w, "[IsFlex]: %v\n", appengine.IsFlex())
+  fmt.Fprintf(w, "[IsStandard]: %v\n", appengine.IsStandard())
+  fmt.Fprintf(w, "[ModuleName]: %v\n", appengine.ModuleName(c))
+  fmt.Fprintf(w, "[RequestID]: %v\n", appengine.RequestID(c))
+  fmt.Fprintf(w, "[ServerSoftware]: %v\n", appengine.ServerSoftware())
+  serviceAccount, _ := appengine.ServiceAccount(c)
+  fmt.Fprintf(w, "[ServiceAccount]: %v\n", serviceAccount)
+  fmt.Fprintf(w, "[VersionID]: %v\n", appengine.VersionID(c))
 }
