eb12931f485d4fe3531cffdb134093c1c6c3c94e
KamiteAkira0611 <kamiteakira@gmail.com>
2021-08-19 17:00:04 +0900

    update

--- golang/model/task.go
+++ golang/model/task.go
@@ -1,31 +1,41 @@
 package model
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/mjibson/goon"
+	"google.golang.org/appengine/datastore"
 )
 
 type Task struct {
+	ID int64 `datastore:"-" goon:"id"`
 	Body  string
 }
 
 func GetTask(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintln(w, "Hello, GAE/go GetTasks")
-	
+	n := goon.NewGoon(r)
+	list := []Task{}
+	q := datastore.NewQuery("Task")
+
+	_, err := n.GetAll(q, &list)
+	if err != nil {
+		w.Write([]byte(fmt.Sprintf(`{"error": "Failed save config: %s"}`, err.Error())))
+		return
+	}
+	json.NewEncoder(w).Encode(list)
 }
 
 func CreateTask(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintln(w, "Hello, GAE/go CreateTasks")
-	
 	n := goon.NewGoon(r)
 	g := &Task{Body: "name"}
+
 	key, err := n.Put(g)
 	if err != nil {
 		w.Write([]byte(fmt.Sprintf(`{"error": "Failed save config: %s"}`, err.Error())))
 		return
 	}
-	w.Write([]byte(fmt.Sprintf(`{"key": "%s"}`, key)))
+	json.NewEncoder(w).Encode(key)
 }
 
