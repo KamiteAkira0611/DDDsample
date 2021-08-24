6c271802281b855781137cf86bff135efd5b528b
KamiteAkira0611 <kamiteakira@gmail.com>
2021-08-24 13:46:11 +0900

    save

--- golang/internal/taskserver/server.go
+++ golang/internal/taskserver/server.go
@@ -8,13 +8,41 @@ import (
 // Server implements the Haberdasher service
 type TaskServer struct{}
 
+type Task struct {
+	ID int64 `datastore:"-" goon:"id"`
+	Body string 
+}
+
+// var w http.ResponseWriter
+// var r *http.Request
+
 func (s *TaskServer) ListTask(ctx context.Context, req *task.ListTaskReq) (resp *task.ListTaskResp, err error) {
+
+	// n := goon.NewGoon(r)
+	// tasks := []Task{}
+	// q := datastore.NewQuery("Task")
+
+	// aaa, err := n.GetAll(q, &tasks)
+	// if err != nil {
+	// 	return nil, twirp.NewError(twirp.Internal, err.Error())
+	// }
+
+	// fmt.Fprintf(w, "invalid request. %s", aaa)
+	// fmt.Fprintf(w, "invalid request. %s", err.Error())
+
 	return &task.ListTaskResp{}, nil
 }
-func (s *TaskServer) CreateTask(ctx context.Context, req *task.ListTaskReq) (resp *task.Task, err error) {
-	// req.GetBody()
-	return &task.Task{}, nil
+
+func (s *TaskServer) CreateTask(ctx context.Context, req *task.CreateTaskReq) (resp *task.Task, err error) {
+	return &task.Task{
+		Id: 111,
+		Body: "createTask " + req.GetBody(),
+	}, nil
 }
-func (s *TaskServer) DeleteTask(ctx context.Context, req *task.ListTaskReq) (resp *task.Task, err error) {
-	return &task.Task{}, nil
+
+func (s *TaskServer) DeleteTask(ctx context.Context, req *task.DeleteTaskReq) (resp *task.Task, err error) {
+	return &task.Task{
+		Id: req.GetId(),
+		Body: "deleteTask",
+	}, nil
 }
--- golang/model/task.go
+++ golang/model/task.go
@@ -10,6 +10,30 @@ import (
 	"google.golang.org/appengine/datastore"
 )
 
+//  context.Background.WithValue(
+// 	&http.contextKey{name:"http-server"},
+// 	&http.Server{Addr:"127.0.0.1:16760",
+// 	Handler:(http.HandlerFunc)(0x7e7a50),
+// 	TLSConfig:(*tls.Config)(0xc00008ac00),
+// 	ReadTimeout:0,
+// 	ReadHeaderTimeout:0,
+// 	WriteTimeout:0,
+// 	IdleTimeout:0,
+// 	MaxHeaderBytes:0,
+// 	TLSNextProto:map[string]func(*http.Server, *tls.Conn, http.Handler){"h2":(func(*http.Server, *tls.Conn, http.Handler))(0x6888e0)},
+// 	ConnState:(func(net.Conn, http.ConnState))(nil),
+// 	ErrorLog:(*log.Logger)(nil),
+// 	disableKeepAlives:0,
+// 	inShutdown:0,
+// 	nextProtoOnce:sync.Once{m:sync.Mutex{state:0, sema:0x0}, done:0x1},
+// 	nextProtoErr:error(nil),
+// 	mu:sync.Mutex{state:0, sema:0x0},
+// 	listeners:map[*net.Listener]struct {}{(*net.Listener)(0xc0002b3400):struct {}{}},
+// 	activeConn:map[*http.conn]struct {}{(*http.conn)(0xc0000bef00):struct {}{}},
+// 	doneChan:(chan struct {})(nil), onShutdown:[]func(){(func())(0x6918c0)}}).WithValue(&http.contextKey{name:"local-addr"},
+// 	&net.TCPAddr{IP:net.IP{0x7f, 0x0, 0x0, 0x1}, Port:16760, Zone:""}).WithCancel.WithCancel.WithValue((*string)(0xd8ca10), &internal.context{req:(*http.Request)(0xc000304000), outCode:0, outHeader:http.Header{}, outBody:[]uint8(nil), pendingLogs:struct { sync.Mutex; lines []*log.UserAppLogLine; flushes int }{Mutex:sync.Mutex{state:0, sema:0x0}, lines:[]*log.UserAppLogLine(nil), flushes:0}, apiURL:(*url.URL)(0xc0002e4080)}).WithValue(3, "twirp.task").WithValue(2, "API").WithValue(6, &internal.context{req:(*http.Request)(0xc000304000), outCode:0, outHeader:http.Header{}, outBody:[]uint8(nil), pendingLogs:struct { sync.Mutex; lines []*log.UserAppLogLine; flushes int }{Mutex:sync.Mutex{state:0, sema:0x0}, lines:[]*log.UserAppLogLine(nil), flushes:0}, apiURL:(*url.URL)(0xc0002e4080)}).WithValue(1, "ListTask")
+
+
 type Task struct {
 	ID int64 `datastore:"-" goon:"id"`
 	Body string 
