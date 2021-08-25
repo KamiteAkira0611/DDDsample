717396bc40ad5c3b827bc410a191c74f3f02c18e
KamiteAkira0611 <kamiteakira@gmail.com>
2021-08-25 09:30:30 +0900

    update

--- golang/internal/taskserver/server.go
+++ golang/internal/taskserver/server.go
@@ -3,6 +3,10 @@ package taskserver
 import (
 	"context"
 	"dddsample/rpc/task"
+
+	"github.com/mjibson/goon"
+	"github.com/twitchtv/twirp"
+	"google.golang.org/appengine/datastore"
 )
 
 // Server implements the Haberdasher service
@@ -18,22 +22,42 @@ type Task struct {
 
 func (s *TaskServer) ListTask(ctx context.Context, req *task.ListTaskReq) (resp *task.ListTaskResp, err error) {
 
-	// n := goon.NewGoon(r)
-	// tasks := []Task{}
-	// q := datastore.NewQuery("Task")
+	n := goon.FromContext(ctx)
+	tasks := []Task{}
+	q := datastore.NewQuery("Task")
 
-	// aaa, err := n.GetAll(q, &tasks)
-	// if err != nil {
-	// 	return nil, twirp.NewError(twirp.Internal, err.Error())
-	// }
+	_, err = n.GetAll(q, &tasks)
+	if err != nil {
+		return nil, twirp.NewError(twirp.Internal, err.Error())
+	}
 
-	// fmt.Fprintf(w, "invalid request. %s", aaa)
-	// fmt.Fprintf(w, "invalid request. %s", err.Error())
+	var items []*task.Task
 
-	return &task.ListTaskResp{}, nil
+	for _, s := range tasks {
+		item := task.Task{
+			Id: s.ID,
+			Body: s.Body,
+		}
+		items = append(items, &item)
+	}
+
+	result := &task.ListTaskResp{Tasks: items}
+	return result, nil
 }
 
 func (s *TaskServer) CreateTask(ctx context.Context, req *task.CreateTaskReq) (resp *task.Task, err error) {
+	// n := goon.FromContext(ctx)
+	// body := req.GetBody()
+
+	// g := &Task{Body: body}
+
+	// key, err := n.Put(g)
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusBadRequest)
+	// 	fmt.Fprintf(w, "invalid request. %s", err.Error())
+	// 	return
+	// }
+	
 	return &task.Task{
 		Id: 111,
 		Body: "createTask " + req.GetBody(),
